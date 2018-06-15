e9932d4bcb2d888227c5218a643991bdc5b248eb
Sirui Chen <632882184@qq.com>
2018-06-15 21:34:49 +0800

    新建两个数据库表：friendship和friendRequest

--- mysql_init_scripts/1_create_tables.sql
+++ mysql_init_scripts/1_create_tables.sql
@@ -6,4 +6,40 @@ CREATE TABLE IF NOT EXISTS `fastchat_db`.`users` (
   `gender` ENUM('male', 'female') NOT NULL,
   PRIMARY KEY (`userName`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC))
-ENGINE = InnoDB
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `fastchat_db`.`friendship` (
+  `userName` VARCHAR(16) NOT NULL,
+  `friendName` VARCHAR(16) NOT NULL,
+  PRIMARY KEY (`userName`, `friendName`),
+  INDEX `fk_friendship_2_idx` (`friendName` ASC),
+  CONSTRAINT `fk_friendship_1`
+    FOREIGN KEY (`userName`)
+    REFERENCES `fastchat_db`.`users` (`userName`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_friendship_2`
+    FOREIGN KEY (`friendName`)
+    REFERENCES `fastchat_db`.`users` (`userName`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `fastchat_db`.`friendRequest` (
+  `from` VARCHAR(16) NOT NULL,
+  `to` VARCHAR(16) NOT NULL,
+  `time` DATETIME NOT NULL,
+  `state` ENUM('rejected', 'accepted', 'pending') NOT NULL,
+  PRIMARY KEY (`from`, `to`),
+  INDEX `fk_friendRequest_2_idx` (`to` ASC),
+  CONSTRAINT `fk_friendRequest_1`
+    FOREIGN KEY (`from`)
+    REFERENCES `fastchat_db`.`users` (`userName`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_friendRequest_2`
+    FOREIGN KEY (`to`)
+    REFERENCES `fastchat_db`.`users` (`userName`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
